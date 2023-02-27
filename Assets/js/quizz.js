b71bd1dd5430d89d8c134e31af74e4d969d5f890
Al Hernandez <awessonn@gmail.com>
2023-02-26 20:47:59 -0800

    added questions to js file.

--- Assets/js/quizz.js
+++ Assets/js/quizz.js
@@ -3,9 +3,9 @@
 
 function headChange(){
     
-    var timely = 60;
+    var timely = 61;
     var  timeInterval = setInterval(function(){
-        
+        timely--;
         if(timely%2 === 0){
             headerEL.setAttribute("style", "color: Yellow");
 
@@ -13,9 +13,9 @@ function headChange(){
         if(timely%2 !== 0){
             headerEL.setAttribute("style", "color: Red");
         }
-        timely--;
+        
 
     }, 1000)
 
 }*/
-
+var questionz = [['There are 2 test tubes placed within a chemical dispenser. The first nozzel dispenses 3mL of chemical at a time, and the second nozzel despenses 2mL at a time. Both nozzles can take away 1mL of chemical from the test tubes at a time. Which pattern of dispensing and taking is correct to get both test tubes to 4mL?'],['You can only enable one rail path. Which path should you enable to get the train from point A to point B?'],[],[],[]];
