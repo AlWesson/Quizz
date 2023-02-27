edf3554b344f710d13526a3c3c0f7dbf0fabf0e2
Al Hernandez <awessonn@gmail.com>
2023-02-26 19:45:56 -0800

    test.

---
+++ Assets/js/quizz.js
@@ -0,0 +1,20 @@
+var headerEL = document.getElementById('header');
+
+
+function headChange(){
+    
+    var timely = 60;
+    var  timeInterval = setInterval(function(){
+        
+        if(timely%2 === 0){
+            headerEL.setAttribute("style", "color: Yellow");
+
+        }
+        if(timely%2 !== 0){
+            headerEL.setAttribute("style", "color: Red");
+        }
+        timely--;
+
+    }, 1000)
+
+}
