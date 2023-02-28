a9a738f12a4081f46fcc3a482bef80181fe48bdb
Al Hernandez <awessonn@gmail.com>
2023-02-28 10:48:29 -0800

    created array of img, question, choices, and correct answer. Also, attempting to add sound files.

--- Assets/js/quizz.js
+++ Assets/js/quizz.js
@@ -2,12 +2,18 @@ let quest = document.querySelector("p"); // loading questionz array here.
 let start = document.getElementById("Start-Quizz");
 let timerEl = document.querySelector(".timer");
 let nextQuestion = 0; // questionz array index;
-let remainingTime = 61;
-// This will be my array of the text portion of the questions.
-let questionz = [['There are 2 test tubes placed within a chemical dispenser. The first nozzel dispenses 3mL of chemical at a time, and the second nozzel despenses 2mL at a time. Both nozzles can take away 1mL of chemical from the test tubes at a time. Which pattern of dispensing and taking is correct to get both test tubes to 4mL?'],['You can only enable one rail path. Which path should you enable to get the train from point A to point B?'],[''],[''],['']];
+let remainingTime = 5;
+let endSound;
 
-// This array will hold the image portions of the questions. 
-// var pics = [[],[],[],[],[]]; 
+
+// This will be my array holding img, question text, answer selection, and correct answer.
+// input type="radio" can be used when the user must select one of multiple button choices.
+let questionz = [{pic: ' [image here] ', question: 'There are 2 test tubes placed within a chemical dispenser. The first nozzel dispenses 3mL of chemical at a time, and the second nozzel despenses 2mL at a time. Both nozzles can take away 1mL of chemical from the test tubes at a time. Which pattern of dispensing and taking is correct to get both test tubes to 4mL?', optionz: ['a','b','c','d'], correctAnswer: 1}, {question: 'You can only enable one rail path. Which path should you enable to get the train from point A to point B?'},{},{},{}];
+
+function playThis(){
+    endSound = new Audio("cuckoo_clock1_x.wav");
+    endSound.play();
+}
 
 
 function setTimer() { 
@@ -18,6 +24,7 @@ function setTimer() {
         if(remainingTime === 0) {
             clearInterval(timerInterval);
             timeDone();
+            playThis();
         }
 
     }, 1000);
