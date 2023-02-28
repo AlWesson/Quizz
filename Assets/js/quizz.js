0dc298924951db4e8760abf6ba6c6d14eff0be5e
Al Hernandez <awessonn@gmail.com>
2023-02-27 17:35:12 -0800

    Working on timer function in js.

--- Assets/js/quizz.js
+++ Assets/js/quizz.js
@@ -1,21 +1,35 @@
-/*var headerEL = document.getElementById('header');
+var quest = document.querySelector("p"); // loading questionz array here.
+var start = document.querySelector("#Start-Quizz");
+var timerEl = document.querySelector(".timer");
+var nextQuestion = 0; // questionz array index;
+var remainingTime = 60;
+// This will be my array of the text portion of the questions.
+var questionz = [['There are 2 test tubes placed within a chemical dispenser. The first nozzel dispenses 3mL of chemical at a time, and the second nozzel despenses 2mL at a time. Both nozzles can take away 1mL of chemical from the test tubes at a time. Which pattern of dispensing and taking is correct to get both test tubes to 4mL?'],['You can only enable one rail path. Which path should you enable to get the train from point A to point B?'],[''],[''],['']];
+
+// This array will hold the image portions of the questions. 
+// var pics = [[],[],[],[],[]]; 
 
 
-function headChange(){
-    
-    var timely = 61;
-    var  timeInterval = setInterval(function(){
-        timely--;
-        if(timely%2 === 0){
-            headerEL.setAttribute("style", "color: Yellow");
-
+function setTimer() {
+    var timerInterval = setInterval(function() {
+        remainingTime--;
+        timerEl.textContent = "Timer: " + remainingTime;
+        if(remainingTime === 0) {
+            clearInterval(timerInterval);
+            timeDone();
         }
-        if(timely%2 !== 0){
-            headerEL.setAttribute("style", "color: Red");
-        }
-        
 
-    }, 1000)
+    }, 1000);
 
-}*/
-var questionz = [['There are 2 test tubes placed within a chemical dispenser. The first nozzel dispenses 3mL of chemical at a time, and the second nozzel despenses 2mL at a time. Both nozzles can take away 1mL of chemical from the test tubes at a time. Which pattern of dispensing and taking is correct to get both test tubes to 4mL?'],['You can only enable one rail path. Which path should you enable to get the train from point A to point B?'],[],[],[]];
+}
+
+function timeDone() {
+    timerEl.textContent = "TIME IS UP!!";
+}
+
+
+start.addEventListener("click" , function() {
+    setTimer();
+
+} 
+);
