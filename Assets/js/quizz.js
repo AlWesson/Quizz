95515ff9ad458657d2c3042cb7241de2fd69f5b0
Al Hernandez <awessonn@gmail.com>
2023-03-05 10:31:38 -0800

    added some visuals to the quiz web page.

--- Assets/js/quizz.js
+++ Assets/js/quizz.js
@@ -1,36 +1,56 @@
-let quest = document.querySelector(".quizzContents"); // loading questionz array here.
+let quest = document.querySelector("#quizzContents"); 
 let start = document.getElementById("Start-Quizz");
 let timerEl = document.getElementById("timer");
-let nextQuestion = 0; // questionz array index;
+//let quezEl = document.getElementById('quez'); // question header
+let parentOfPic = document.getElementById("picGoesAfterThis"); // parent of where I'll dynamically place my images.
+let quizzPic = document.createElement('img'); 
+let textPortionEl = document.getElementById('textPortion'); // text portion of question.
+let multiChoiceEl = document.querySelectorAll('.multiChoice'); // radio button group class.
+
+// All the label ids for my radio buttons.
+let A = document.getElementById('_A');
+let B = document.getElementById('_B');
+let C = document.getElementById('_C');
+let D = document.getElementById('_D');
+
+let questionNumber = 0; // questionz array index;
 let remainingTime = 61;
 let timerResult = remainingTime; //variable I'll use to edit time.
-let endSound;
-let quizzPic = document.createElement("img"); // creates an image element.
+let endSound; // time's up sound.
 
 
 // This will be my array holding img, question text, answer selection, and correct answer.
-// input type="radio" can be used when the user must select one of multiple button choices.
-let questionz = [{pic: ' [image here] ', question: 'There are 2 test tubes placed within a chemical dispenser. The first nozzel dispenses 3mL of chemical at a time, and the second nozzel despenses 2mL at a time. Both nozzles can take away 1mL of chemical from the test tubes at a time. Which pattern of dispensing and taking is correct to get both test tubes to 4mL?', optionz: ['a','b','c','d'], correctAnswer: 1}, {pic: ' [image here] ', question: 'You can only enable one rail path. Which path should you enable to get the train from point A to point B?', optionz: ['a','b','c','d'], correctAnswer: 2 },{pic: '[ image here ]', question: ' ', optionz: ['a','b','c','d'], correctAnswer: 0}];
+let questionz = [{pic: "./Assets/images/cat.jpg", question: 'There are 2 test tubes placed within a chemical dispenser. The first nozzel dispenses 3mL of chemical at a time, and the second nozzel despenses 2mL at a time. Both nozzles can withdraw 1mL of chemical from the test tubes at a time. Which pattern of dispensing and withdrawing is correct to get both test tubes to 4mL?', a:"", b:"", c:"", d:"", correctAnswer: 'b'}, {pic: ' [image here] ', question: 'You can only enable one rail path. Which path should you enable to get the train from point A to point B?', a:"", b:"", c:"",d:"", correctAnswer: 'c' },{pic: '[ image here ]', question: ' ', a:"", b:"", c:"", d:"", correctAnswer: 'a'}];
 
-// Hides element
+// Hides elements function
 function toHide(aSelection) {
     let a = document.getElementById(aSelection);
     a.style.display = 'none';
 }
-// Reveals element
+// Reveals elements function
 function toShow(bSelection) {
     let b = document.getElementById(bSelection);
     b.style.display = 'block';
 }
+
+
 // function to set a sound file path to the endSound let. It then commands endSound to play the sound file.
 function playThis(){
     endSound = new Audio("./Assets/sound/cuckoo_clock1_x.wav");
     endSound.play();
 }
 // Function to pull from the array of objects and placed them into designated location on page.
-function everythingNeeded (intake) {
-    
+function quizMaterial () {
 
+    let quizArray = questionz[questionNumber];
+    quizzPic.setAttribute('src', quizArray.pic);
+    quizzPic.setAttribute("style", "max-width: 720px");
+    parentOfPic.prepend(quizzPic);
+    textPortionEl.innerHTML = quizArray.question;
+    _A.innerHTML = quizArray.a;
+    _B.innerHTML = quizArray.b;
+    _C.innerHTML = quizArray.c;
+    _D.innerHTML = quizArray.d;
 }
 
 // This fucntion sets the timer and starts it when called.
@@ -62,12 +82,13 @@ function timeDone() {
 
 // this will call most of the functions on start event listener.
 function startUpQuizz(event) {
-    
+    event.preventDefault();
     toHide('Start-Quizz');
     toShow('quizzContents');
     toShow('timer');
     setTimer();
-    event.preventDefault();
+    quizMaterial();
+    
 
 }
 
